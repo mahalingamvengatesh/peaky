33f1a1039df0c475c13dbe99d77c403d0cc4d9e9
mahalingamvengatesh <mahalingamvengatesh@gmail.com>
2022-07-02 19:58:05 +0530

    welcome shanthi code

--- src/org/sam/TestGit.java
+++ src/org/sam/TestGit.java
@@ -1,6 +1,9 @@
 package org.sam;
 
 public class TestGit {
+	private void tc2() {
+     System.out.println("shanthi done the coding");
+	}
 	public static void main(String[] args) {
 		System.out.println("git is good");
 	}
